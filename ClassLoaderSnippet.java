46e02b92d4b7ebe282da3e211ae1e55ec04b6d8f
fuzzyjulz <julz.west@gmail.com>
2015-04-15 14:17:15 +1000

    Update ClassLoaderSnippet.java

--- ClassLoaderSnippet.java
+++ ClassLoaderSnippet.java
@@ -33,3 +33,100 @@
   }
   
  }
+ 
+ /******************* OR ****************************************/
+ 
+ 
+ 
+     /** Add in all classes as handlers for the given package */
+    private List<Class<?>> getClasses(String pckg)
+    {
+        List<Class<?>> handlers = new ArrayList<Class<?>>();
+        URL root = Thread.currentThread().getContextClassLoader().getResource(pckg.replace(".", "/"));
+        String rootPath = root.getFile();
+        List<String> classes;
+        
+        if (rootPath.contains("!"))
+        {
+            classes = getClassNamesFromJar(root);
+        } else {
+            classes = getClassNamesFromDirectory(pckg, rootPath);
+        }
+
+        ClassLoader loader = this.getClass().getClassLoader();
+        
+        for (String className : classes)
+        {
+            try
+            {
+                Class<?> cls = loader.loadClass(className);
+    
+                if (AppRequestManager.class.isAssignableFrom(cls)) {
+                    handlers.add(cls);
+                }
+            }
+            catch (ClassNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        
+        return handlers;
+    }
+    
+    /** Look up the basic directory structure and pull out all classes that are in the directory structure.*/
+    private List<String> getClassNamesFromDirectory(String pckg, String rootPath)
+    {
+        ArrayList<String> classes = new ArrayList<String>();
+        
+        File[] files = new File(rootPath).listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".class");
+            }
+        });
+
+        for (File file : files) 
+        {
+            String className = file.getName().replaceAll(".class$", "");
+
+            classes.add(pckg + "." + className);
+        }
+
+        return classes;
+    }
+
+    /** Look into the JAR and pull out all entries for the given directory*/
+    private List<String> getClassNamesFromJar(URL jarDirectory) 
+    {
+        ArrayList<String> classes = new ArrayList<String>();
+        try 
+        {
+            JarURLConnection jarConnection = (JarURLConnection)jarDirectory.openConnection();
+            JarFile jar = jarConnection.getJarFile();
+            try 
+            {
+                String jarPath = jarConnection.getEntryName();
+                
+                Enumeration<JarEntry> entries = jar.entries();
+                while (entries.hasMoreElements()) {
+                    JarEntry entry = entries.nextElement();
+                    if (entry.getName().startsWith(jarPath) && !entry.isDirectory()) 
+                    {
+                        String className = entry.getName().replace("/", ".").replace(".class","");
+                        classes.add(className);
+                    }
+                }
+            }
+            finally 
+            {
+                try { jar.close(); } catch (Exception e) {}
+            }
+
+            return classes;
+        } catch (IOException e) 
+        {
+            throw new RuntimeException("Can't open Jar", e);
+        }
+    }
+ 
+ 
